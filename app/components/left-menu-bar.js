b0d5c94617dcfb9d6228e0a02e094ad00487ec6b
Alex Makhov <xHisusx@gmail.com>
2018-04-18 20:20:17 +0300

    upgrade side-basket and modal dialog

--- app/components/left-menu-bar.js
+++ app/components/left-menu-bar.js
@@ -1,4 +1,14 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  actions: {
+    openSideMenu: function() {
+      document.getElementById('side-menu').style.width = '300px';
+      document.getElementById("dimmer").style.display = 'block';
+    },
+    closeSideMenu: function() {
+      document.getElementById('side-menu').style.width = '0px';
+      document.getElementById("dimmer").style.display = 'none';
+    }
+  }
 });
--- app/components/side-basket.js
+++ app/components/side-basket.js
@@ -1,4 +1,23 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+
+  actions: {
+    openModalDialog: function(){
+      document.getElementById("modalDialog").style.display = "flex";
+
+    },
+    closeModalDialog: function(){
+      document.getElementById("modalDialog").style.display = "none";
+    },
+    choosePlan: function() {
+      //TODO ever choice
+      document.getElementById("deliver-table").style.display = "none";
+      document.getElementById("data-table").style.display = "flex";
+    },
+    createOrder: function() {
+      document.getElementById("data-table").style.display = "none";
+      document.getElementById("info-table").style.display = "flex";
+    }
+  }
 });
